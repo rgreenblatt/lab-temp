d6bd89c1b8a7dcab28fd98b1e642aca6a6da42a8
rgreenblatt <greenblattryan@gmail.com>
2021-01-29 15:15:11 -0500

    Finished exercises

--- more_functions/exercises.ml
+++ more_functions/exercises.ml
@@ -1,16 +1,16 @@
 (* Exercise: Compose*)
-let compose f g x= f (g x)
+let compose f g x = f (g x)
 
 (* Exercise: Power of Fold *)
 
 (* Function length takes in a list and returns it's length*)
-let length l = List.length l
+let length l = List.fold_left (fun v _ -> v + 1) 0 l
 
 (* Function rev reverses a given list *)
-let rev l = List.rev l
+let rev l = List.fold_left (fun xs x -> x :: xs) [] l
 
 (* Map takes in a function f, list l and applies f to each element in l*)
-let map f l = List.map f l
+let map f l = List.fold_right (fun x xs -> f x :: xs) l []
 
 (* Filter removes the elements that doesn't safisy the given precidate f*)
-let filter f l = List.filter f l
+let filter f l = List.fold_right (fun x xs -> if f x then x :: xs else xs) l []
